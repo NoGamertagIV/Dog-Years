0de0b4cff77e6ed93b3958fe0537059b42715923
NoGamertagIV <148359018+NoGamertagIV@users.noreply.github.com>
2024-01-30 07:50:03 -0500

    Add files via upload

---
+++ dog-years.js
@@ -0,0 +1,10 @@
+const myAge = 32
+let earlyYears = 2
+// variable that will change //
+earlyYears *= 10.5;
+let laterYears = myAge - 2; 
+// taking two years from my age //
+laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Dylan Barga'.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
